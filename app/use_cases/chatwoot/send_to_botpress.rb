d8335c1e730e09b34e11edfce217935213add7b8
Julio Cezar Oidella <juliooidella@gmail.com>
2023-12-21 15:48:06 -0300

    Update send_to_botpress.rb

--- app/use_cases/chatwoot/send_to_botpress.rb
+++ app/use_cases/chatwoot/send_to_botpress.rb
@@ -7,26 +7,40 @@ class Chatwoot::SendToBotpress < Micro::Case
 
   def call!
     conversation_id = event['conversation']['id']
-    message_content = event['content']
     url = "#{botpress_endpoint}/api/v1/bots/#{botpress_bot_id}/converse/#{conversation_id}"
 
     body = {
-      'text': "#{message_content}",
       'type': 'text',
       'metadata': {
         'event': event
       }
     }
 
-    response = Faraday.post(url, body.to_json, {'Content-Type': 'application/json'})
+    if event['event'] == 'message_updated' &&
+       event['content_type'] == 'input_select' &&
+       event['content_attributes'].key?('submitted_values')
+      submitted_values = event['content_attributes']['submitted_values']
+      if submitted_values.is_a?(Array) && !submitted_values.empty?
+        # Assuming that 'Title' is always present in the first element of 'submitted_values' array
+        body['text'] = submitted_values[0]['title']
+      else
+        # Handle the case where 'submitted_values' is empty or not an array
+        Failure result: { message: 'Invalid submitted_values' }
+      end
+    else
+      # For other events, use the original message content
+      body['text'] = event['content']
+    end
+
+    response = Faraday.post(url, body.to_json, { 'Content-Type': 'application/json' })
 
     Rails.logger.info("Botpress response")
     Rails.logger.info("Status code: #{response.status}")
     Rails.logger.info("Body: #{response.body}")
 
-    if (response.status == 200)
+    if response.status == 200
       Success result: JSON.parse(response.body)
-    elsif (response.status == 404 && response.body.include?('Invalid Bot ID'))
+    elsif response.status == 404 && response.body.include?('Invalid Bot ID')
       Failure result: { message: 'Invalid Bot ID' }
     else
       Failure result: { message: 'Invalid botpress endpoint' }
